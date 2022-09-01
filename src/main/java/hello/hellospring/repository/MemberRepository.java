19fb738bc34093ad09fd0f241d86a1bd9b4fcbd7
ro117youshinKIM <livebyfaith117@naver.com>
2022-09-01 16:02:33 +0900

    Repository 네이밍

--- src/main/java/hello/hellospring/repository/MemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -5,6 +5,7 @@ import hello.hellospring.domain.Member;
 import java.util.List;
 import java.util.Optional;
 
+// repository는 데이터의 흐름과 같이 기계적인 네이밍으로 개발해야 한다.
 public interface MemberRepository {
     Member save(Member member);
     Optional<Member> findById(Long id);
